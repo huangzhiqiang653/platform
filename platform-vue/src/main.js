2d964c20a716a065653845084303562a50478c66
李鹏军 <939961241@qq.commkdir>
2017-12-19 23:34:41 +0800

    VUE静态页DEMO

--- platform-vue/src/main.js
+++ platform-vue/src/main.js
@@ -3,13 +3,19 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import YDUI from 'vue-ydui';
+import 'vue-ydui/dist/ydui.px.css';
+import {ActionSheet} from 'vue-ydui/dist/lib.px/actionsheet'
 
 Vue.config.productionTip = false
+
+Vue.component(ActionSheet.name, ActionSheet);
+Vue.use(YDUI);
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: {App}
+  components: { App }
 })
